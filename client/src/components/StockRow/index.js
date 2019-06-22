56c54ce5b7818eb2f6046673e47b7f53dc6e2b25
CJ Vaughn <cjvaughn@rocketmail.com>
2019-06-22 14:29:12 -0700

    Set up API data pull and organized results into table

---
+++ client/src/components/StockRow/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+class StockRow extends Component {
+
+    render() {
+        return (
+            <tr key={this.props.name}>
+                <td>{this.props.name}</td>
+                <td>${this.props.price}</td>
+                <td><Button as="input" type="button" value="Buy" /></td>
+            </tr>
+        )
+    }
+}
+
+export default StockRow
--- client/src/pages/StockList/index.js
+++ client/src/pages/StockList/index.js
@@ -1,13 +1,77 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import StockRow from '../../components/StockRow';
+import Container from 'react-bootstrap/Container';
 
 class StockList extends Component {
 
-    render(){
-        return(
-            <p>Get your list here.</p>
-        )
+    state = {
+        error: null,
+        stockInfo: [],
+        isLoaded: false
     }
+
+    componentDidMount() {
+
+        fetch('https://api.worldtradingdata.com/api/v1/stock_search?stock_exchange=NASDAQ&api_token=AbfOZS7KraxAILz0rqzgIfOJHbTIxxEX8upEqHnj1CeCphNBXrm60jcDug8W')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        stockInfo: result.data
+                    });
+
+                    console.log(result.data);
+
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+
+        const { error, isLoaded, stockInfo } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <Container>
+                    <table className="table table-striped border border-rounded border-primary">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Purchase</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {stockInfo.map(stock => (
+
+                                <StockRow
+                                    name={stock.name}
+                                    price={stock.price}
+                                />
+
+                            ))}
+
+                        </tbody>
+                    </table>
+                </Container>
+            );
+        }
+    }
+
 }
 
 export default StockList;
